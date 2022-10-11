66e6f2a60d09f9a94d71ecaacefdb2b8f46815e1
Samuel Gaddis <siddag14@live.com>
2022-10-11 17:30:26 -0400

    commit tree 43aac20ef62ab98c1ac9c5b56e7ba32f7707b9f0

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    const hqBlock = 42;
+    let difference = pickUp - hqBlock;
+    let numberOfBlocks = Math.abs(difference);
+    return numberOfBlocks;
+    
+};
+
+//function distanceFromHqInFeet(numberOfBlocks)
+function distanceFromHqInFeet(blocksToFeet) {
+    let blockDifferenceResults = distanceFromHqInBlocks(blocksToFeet);
+    let totalFeet = blockDifferenceResults * 264;
+    return totalFeet;
+}
+console.log(distanceFromHqInFeet(41));
+
+function distanceTravelledInFeet(start, destination) {
+    let distance = destination - start;
+    let absoluteDistance = Math.abs(distance);
+    let distanceTravelledInFeet = absoluteDistance * 264;
+    return distanceTravelledInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    let totalFeet = (destination - start) *264;
+        if (totalFeet <= 400) {
+            return 0;
+        } else if (totalFeet > 400 && totalFeet <= 2000) {
+            return .02 * (totalFeet - 400);
+        } else if (totalFeet > 2000 && totalFeet <= 2500) {
+            return 25;
+        } else {
+            return 'cannot travel that far';
+        }
+}
+console.log(calculatesFarePrice(18,34));
